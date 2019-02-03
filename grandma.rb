12069d8afb572e0bcef615f7a92703dfee09788a
John Pena <jpena91@gmail.com>
2019-02-03 18:53:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,11 @@
 def speak_to_grandma(phrase)
-  if phrase !== phrase.Upcase
-    puts"NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
+  if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  else 
+  elsif phrase !== phrase.Upcase
     puts "HUH!? SPEAK UP, SONNY!"
+  else 
+    puts "NO, NOT SINCE 1938!"
 end
+
+
+stg("HELLO!")
